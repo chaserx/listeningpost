bf0d8f51abc544af0454cc6ec6d81945513e5b33
Chase Southard <chase.southard@gmail.com>
2015-07-14 11:02:08 -0400

    add callback associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,17 @@
 class Message < ActiveRecord::Base
   belongs_to :device
   validates :device, presence: true
+
+  after_create :send_callback
+
+  private
+
+  def send_callback
+    if callback=device.callback
+      result = HTTParty.post(callback.url.to_str,
+                             body: { callback.body }.to_json,
+                             headers: { 'Content-Type' => 'application/json' } )
+      # log error; inform user if result not :ok
+    end
+  end
 end
