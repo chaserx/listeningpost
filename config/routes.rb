a88b332eef5022dd4f0a1bf34ff65cc3dc91a58a
Chase Southard <chase.southard@gmail.com>
2015-07-28 22:57:58 -0400

    revert previous decision, include the show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :devices, only: [:create, :destroy, :update, :index] do
-    resources :messages, only: [:create, :destroy, :index]
+  resources :devices, only: [:create, :destroy, :update, :index, :show] do
+    resources :messages, only: [:create, :destroy, :index, :show]
     resources :callbacks, only: [:create, :destroy, :index, :update, :show]
   end
   post 'devices/:id' => 'messages#create'
