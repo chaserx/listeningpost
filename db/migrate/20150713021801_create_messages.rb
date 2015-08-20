9080575ec7aaee34ac65534de289482542326600
Chase Southard <chase.southard@gmail.com>
2015-08-20 00:29:12 -0400

    update migrations to reference user

--- db/migrate/20150713021801_create_messages.rb
+++ db/migrate/20150713021801_create_messages.rb
@@ -2,8 +2,9 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages, id: :uuid do |t|
       t.text :body
-      t.uuid :device_id
+      t.uuid :device_id, index: true
       t.string :ip_address
+      t.uuid :user_id, index: true
       t.timestamps null: false
     end
   end
--- db/migrate/20150730212217_create_webhooks.rb
+++ db/migrate/20150730212217_create_webhooks.rb
@@ -1,8 +1,8 @@
 class CreateWebhooks < ActiveRecord::Migration
   def change
-    create_table :webhooks do |t|
+    create_table :webhooks, id: :uuid do |t|
       t.string :message_body, null: false
-      t.uuid :device_id, null: false
+      t.uuid :device_id, null: false, index: true
       t.string :url, null: false
       t.string :headers
       t.string :access_token
@@ -10,6 +10,7 @@ class CreateWebhooks < ActiveRecord::Migration
       t.json :payload
       t.string :query
       t.string :auth
+      t.uuid :user_id, index: true
       t.timestamps null: false
     end
   end
