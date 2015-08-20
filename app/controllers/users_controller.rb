4ee99c586a3af9bc822e54f117c2a1fbb29f9877
Chase Southard <chase.southard@gmail.com>
2015-08-20 10:08:05 -0400

    limit data returned about a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      render :show, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
