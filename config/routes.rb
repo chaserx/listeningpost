ce28aac44d8d328a31b0283aec94d7db845ae16c
Chase Southard <chase.southard@gmail.com>
2015-08-20 22:35:35 -0400

    add knock to application controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  mount Knock::Engine => "/knock"
+
   resources :devices, only: [:create, :destroy, :update, :index, :show] do
     resources :messages, only: [:create, :destroy, :index, :show]
     resources :callbacks, only: [:create, :destroy, :index, :update, :show]
     resources :webhooks, only: [:create, :destroy, :index, :show, :update]
   end
+
   post 'devices/:id' => 'messages#create'
   post '/users' => 'users#create'
 end
