bae5fefd3156b6a8a3ef452038fafe6640eb6a39
Chase Southard <chase.southard@gmail.com>
2015-08-23 00:18:45 -0400

    add validations for webhook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
+
   has_many :devices
   has_many :messages
   has_many :webhooks
+
+  #TODO(chase): also validate email structure
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 8 }, confirmation: true
 end
