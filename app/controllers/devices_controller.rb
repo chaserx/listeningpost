4b822d6723144f70b97be5330051551fad122821
Chase Southard <chase.southard@gmail.com>
2015-08-23 00:20:00 -0400

    add auth and permit certain attrs

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,18 +2,15 @@ class DevicesController < ApplicationController
   before_action :authenticate
   before_action :set_device, only: [:show, :update, :destroy]
 
-  # GET /devices
   # GET /devices.json
   def index
     @devices = Device.where(user_id: current_user.id)
   end
 
-  # GET /devices/1
   # GET /devices/1.json
   def show
   end
 
-  # POST /devices
   # POST /devices.json
   def create
     @device = Device.new(device_params)
@@ -25,7 +22,6 @@ class DevicesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /devices/1
   # PATCH/PUT /devices/1.json
   def update
     if @device.update(device_params)
@@ -35,7 +31,6 @@ class DevicesController < ApplicationController
     end
   end
 
-  # DELETE /devices/1
   # DELETE /devices/1.json
   def destroy
     if @device.destroy
--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,39 +1,36 @@
 class WebhooksController < ApplicationController
+  before_action :authenticate
+  before_action :set_device, execpt: [:index]
   before_action :set_webhook, only: [:show, :update, :destroy]
 
-  # GET /webhooks
   # GET /webhooks.json
   def index
     @webhooks = Webhook.where(device_id: params[:device_id])
   end
 
-  # GET /webhooks/1
   # GET /webhooks/1.json
   def show
   end
 
-  # POST /webhooks
   # POST /webhooks.json
   def create
     @webhook = Webhook.new(webhook_params)
     if @webhook.save
-      render :show, status: :created, location: @webhook
+      render :show, status: :created, location: [@device, @webhook]
     else
       render json: @webhook.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /webhooks/1
   # PATCH/PUT /webhooks/1.json
   def update
     if @webhook.update(webhook_params)
-      render :show, status: :ok, location: @webhook
+      render :show, status: :ok, location: [@device, @webhook]
     else
       render json: @webhook.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /webhooks/1
   # DELETE /webhooks/1.json
   def destroy
     if @webhook.destroy
@@ -45,6 +42,10 @@ class WebhooksController < ApplicationController
 
   private
 
+  def set_device
+    @device = Device.find(params[:device_id])
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_webhook
     @webhook = Webhook.find(params[:id])
@@ -52,6 +53,7 @@ class WebhooksController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def webhook_params
-    params[:webhook]
+    params.require(:webhook).permit(:url, :request_type, :message_body).
+      merge(device_id: params[:device_id])
   end
 end
