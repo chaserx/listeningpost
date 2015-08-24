66adbb059c156aa92cc5e1818d39218291b25a09
Chase Southard <chase.southard@gmail.com>
2015-08-24 00:06:11 -0400

    remove this callback method. opt for elsewhere

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,19 +5,4 @@ class Message < ActiveRecord::Base
   validates :device, presence: true
   validates :ip_address, presence: true
   validates :user, presence: true
-
-  after_create :send_webhook
-
-  private
-
-  def send_webhook
-    # Note(chaserx): move this to an async queue one day
-    # Update to be webhook not callback
-    # if callback=device.callback
-    #   result = HTTParty.post(callback.url.to_str,
-    #                          body: callback.body.to_json,
-    #                          headers: { 'Content-Type' => 'application/json' } )
-    #   # log error; inform user if result not :ok
-    # end
-  end
 end
