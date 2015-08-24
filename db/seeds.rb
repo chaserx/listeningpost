683902a4a4e973fb192ba27aab7041da8d00d5cd
Chase Southard <chase.southard@gmail.com>
2015-08-23 20:44:04 -0400

    add dev seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+seed_file = ENV['SEED_FILE'] || Rails.env
+path = Rails.root.join('db', 'seeds', "#{seed_file}.rb")
+if File.exist?(path)
+  puts "Seeding from #{path}"
+  require path
+else
+  puts "No seed file at #{path}"
+end
