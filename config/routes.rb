749abafc3c61ba768423e01ca05f16bb0dcc267c
Chase Southard <chase.southard@gmail.com>
2015-09-03 22:57:10 -0400

    simplify routes by using except

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   mount Knock::Engine => '/knock'
 
   resources :users, only: [:create, :show, :update]
-  resources :devices, only: [:create, :destroy, :update, :index, :show] do
+  resources :devices, except: [:new, :edit] do
     resources :messages, only: [:create, :index, :show]
-    resources :webhooks, only: [:create, :destroy, :index, :show, :update]
+    resources :webhooks, except: [:new, :edit]
   end
 end
