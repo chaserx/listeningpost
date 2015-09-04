4ab7278c3f4ef8f429a94757820eccb29c3be89f
Chase Southard <chase.southard@gmail.com>
2015-09-03 22:42:45 -0400

    run thru the style garden

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -39,6 +39,7 @@ class MessagesController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list
   # through.
   def message_params
-    params.require(:message).permit(:body).merge(user_id: current_user.id)
+    params.require(:message).permit(:subject, :body).
+                             merge(user_id: current_user.id)
   end
 end
